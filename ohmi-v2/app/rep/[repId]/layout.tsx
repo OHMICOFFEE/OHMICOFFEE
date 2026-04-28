9db2c5daff5f17597e35cf55d7eea3450e52e5f1
OHMICOFFEE <platinumswealth@gmail.com>
2026-04-28 20:15:52 +0200

    Delete ohmi-v2 directory

--- ohmi-v2/app/rep/[repId]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import RepSidebar from '@/components/rep/Sidebar'
-export default function RepLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen bg-navy">
-      <RepSidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">{children}</div>
-    </div>
-  )
-}
