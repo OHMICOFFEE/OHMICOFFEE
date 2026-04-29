ceb0d91dedd82a71582a75594e4eea810fbebcbe
OHMICOFFEE <platinumswealth@gmail.com>
2026-04-29 12:46:59 +0200

    Add files via upload

---
+++ ohmi-v3/app/admin/layout.tsx
@@ -0,0 +1,4 @@
+import AdminSidebar from '@/components/admin/Sidebar'
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <div className="layout"><AdminSidebar /><div className="main">{children}</div></div>
+}
