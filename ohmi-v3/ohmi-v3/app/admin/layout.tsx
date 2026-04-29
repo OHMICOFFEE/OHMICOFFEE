a0ad1b71dea0f731f88f623ae4e29997301993a5
OHMICOFFEE <platinumswealth@gmail.com>
2026-04-29 16:12:00 +0200

    Delete ohmi-v3 directory

--- ohmi-v3/ohmi-v3/app/admin/layout.tsx
+++
@@ -1,4 +0,0 @@
-import AdminSidebar from '@/components/admin/Sidebar'
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <div className="layout"><AdminSidebar /><div className="main">{children}</div></div>
-}
--- ohmi-v3/ohmi-v3/app/admin/tree/page.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import BinaryTree from '@/components/BinaryTree'
-export default function AdminTree() {
-  return <BinaryTree portalType="admin" />
-}
