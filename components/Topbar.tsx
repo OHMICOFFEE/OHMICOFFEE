85e87960eb3d41a2507476a31f1ca12eb6931e0a
OHMICOFFEE <platinumswealth@gmail.com>
2026-04-30 15:15:21 +0200

    Delete components directory

--- components/Topbar.tsx
+++
@@ -1,8 +0,0 @@
-export default function Topbar({ title, action }: { title: string; action?: React.ReactNode }) {
-  return (
-    <div className="topbar">
-      <span className="topbar-title">{title}</span>
-      {action}
-    </div>
-  )
-}
