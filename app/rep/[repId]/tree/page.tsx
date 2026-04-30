4c2f156d449a2732e392cbadaf3902942abc649f
OHMICOFFEE <platinumswealth@gmail.com>
2026-04-30 15:37:26 +0200

    Delete app directory

--- app/rep/[repId]/tree/page.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import BinaryTree from '@/components/BinaryTree'
-export default function RepTree({ params }: { params: { repId: string } }) {
-  return <BinaryTree portalType="rep" repId={params.repId} />
-}
